70b86b3a35cb556b744229fd6b6162198436e606
SugarSyrup <tlfvm04@naver.com>
2023-08-01 02:22:58 +0900

    Assignment 5

--- src/pages/Todo.js
+++ src/pages/Todo.js
@@ -1,18 +1,41 @@
-import { useEffect } from "react";
+import axios from "axios";
+import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 
 function Todo() {
     const navigate = useNavigate();
+    const [todos, setTodos] = useState([]);
 
     useEffect(() => {
-        if(!localStorage.getItem('accesstoken')) {
+        const access_token = localStorage.getItem('accesstoken');
+        if(!access_token) {
             navigate('/signin')
         }
+        axios.get('https://www.pre-onboarding-selection-task.shop/auth/todos', {
+            headers: {Authorization : `Bearer ${access_token}`}
+        })
+            .then((response) => {
+                setTodos(response.data);
+            })
     }, [])
     
     return(
         <div>
-            Todo
+            <h1>Todos</h1>
+            <div>
+                {
+                    todos.map((todo) => {
+                        return (
+                            <li key={todo.id}>
+                                <label>
+                                    <input type="checkbox" checked={todo.isCompleted}/>
+                                    <span>{todo.todo}</span>
+                                </label>
+                            </li>
+                        )
+                    })
+                }
+            </div>
         </div>
     )
 }
